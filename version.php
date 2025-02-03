0b1799f53f95991b8d73a2a1f0dedb425df5b5a6
Eduardo Kraus <kraus@eduardokraus.com>
2025-02-03 20:49:05 -0300

    V 2.0.4

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025011200;
+$plugin->version = 2025020300;
 $plugin->requires = 2010112400;
-$plugin->release = "2.0.3";
+$plugin->release = "2.0.4";
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = "block_rate";
